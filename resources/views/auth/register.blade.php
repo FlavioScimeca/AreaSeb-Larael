5adf3e300802b6c75561924c1e8546cf2ed196cd
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-04-16 17:40:24 +0200

    finished register logic

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,32 @@
 <x-layout title="Register">
-    <h1 class="text-center">Register</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto p-md-5">
+                <form method="POST" action="{{route("register")}}">
+                    @csrf
+                    <div class="mb-3">
+                      <label for="inputName" class="form-label">Name</label>
+                      <input name="name" type="text" class="form-control" id="inputName" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                      <label for="inputEmail" class="form-label">Email</label>
+                      <input name="email" type="email" class="form-control" id="inputEmail">
+                    </div>
+                    <div class="mb-3">
+                        <label for="inputPassword" class="form-label">Password</label>
+                        <input name="password" type="password" class="form-control" id="inputPassword">
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputPassword2" class="form-label">Repeat password</label>
+                        <input name="password_confirmation" type="password" class="form-control" id="inputPassword2">
+                      </div>
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Diventa un Admin</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Registrati</button>
+                  </form>
+            </div>
+        </div>
+    </div>
 </x-layout>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,35 +1,39 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="{{route("login")}}">Login</a></li>
-              <li><a class="dropdown-item" href="{{route("register")}}">Register</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <button class="btn btn-outline-success">Search</button>
-        </form>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Ciao {{ Auth::check() ? Auth::user()->name : "Guest" }}</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link border-2 border-bottom border-dark" href="#">Profilo</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          @auth
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{route("login")}}">Login</a></li>
+            <li><a class="dropdown-item" href="{{route("register")}}">Register</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+          @else
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{route("login")}}">Login</a></li>
+            <li><a class="dropdown-item" href="{{route("register")}}">Register</a></li>
+          </ul>
+          @endauth
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button class="btn btn-outline-success"><i class="bi bi-plus-circle-fill"></i> articolo</button>
+      </form>
     </div>
-  </nav>
+  </div>
+</nav>
